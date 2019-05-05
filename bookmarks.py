b5dfbd0f02cf1f2035d7e5885e513ef65c57811f
Boris Makarevitch <boris.makarevitch@gmail.com>
2019-05-05 13:34:42 +0300

    Hierarchical bookmarks

--- bookmarks.py
+++ bookmarks.py
@@ -4,36 +4,44 @@ from os import path, environ
 from PyPDF2 import PdfFileMerger, PdfFileReader
 import sys
 import getopt
+import traceback
 
 
 def add_flat(output, lines, offset):
-
     for line in lines:
         *head, tail = line.split()
-        name = ' '.join(head)
+        title = ' '.join(head)
         page = int(tail) + offset
         try:
-            output.addBookmark(name, page, parent=None)
+            output.addBookmark(title, page, None)
         except IndexError:
-            print(name, page)
+            print(line)
+            traceback.print_exc()
             exit(1)
 
 
 def add_hier(output, lines, offset):
-    parent = None
+    marks = {}
     for line in lines:
-        hdr, *tail = line.split()
-        page = tail.pop()
-        title = ' '.join(tail).replace('.', '')
-        page = int(page) + offset
-        print(hdr, title, page)
+        try:
+            hdr, *tail = line.split()
+            hdr = hdr.rstrip('.')
+            page = tail.pop()
+            title = hdr + ' ' + ' '.join(tail).replace('.', '')
+            *head, _ = hdr.split('.')
+            parent = marks['.'.join(head)] if len(head) > 0 else None
+            page = int(page) + offset
+            marks[hdr] = output.addBookmark(title, page, parent)
+        except ValueError:
+            print(line)
+            traceback.print_exc()
+            exit(1)
 
 
-if __name__ == '__main__':
-
+def main(argv):
     struct = 'flat'
     remove = ''
-    offset = 0
+    offset = -1
 
     opts, args = getopt.getopt(sys.argv[1:], 'b:i:o:s:r:')
     for opt, val in opts:
@@ -52,15 +60,14 @@ if __name__ == '__main__':
         if opt == '-r':
             remove = val
 
-    dir = path.join(environ['HOME'], 'pr/data')
-    # lines = []
+    root = path.join(environ['HOME'], 'pr/data/bkm')
 
-    with open(path.join(dir, bkfile)) as f:
+    with open(path.join(root, bkfile)) as f:
         lines = [l.rstrip().replace(remove, '') for l in f.readlines()]
 
     output = PdfFileMerger()
 
-    with open(path.join(dir, infile + '.pdf'), 'rb') as f:
+    with open(path.join(root, infile + '.pdf'), 'rb') as f:
         output.append(PdfFileReader(f), import_bookmarks=False)
 
     if struct == 'flat':
@@ -68,10 +75,9 @@ if __name__ == '__main__':
     else:
         add_hier(output, lines, offset)
 
-# output.addPage(input.getPage(0))
-
-# output.setPageMode("/UseOutlines")
-    with open(path.join(dir, infile + '_out.pdf'), 'wb') as outStream:
+    with open(path.join(root, infile + '_bkm.pdf'), 'wb') as outStream:
         output.write(outStream)
 
 
+if __name__ == '__main__':
+    main(sys.argv)
